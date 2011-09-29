2ef11059bfb9293b82f8f683d87bc695eac05a1a
Maxim Zenin <maxx@adfox.ru>
2011-09-29 17:23:59 +0400

    Little tweaks in File manager

--- app/views/admin/files/table.php
+++ app/views/admin/files/table.php
@@ -7,8 +7,13 @@ foreach ($table as $row) {
 
 	$delete = sprintf("<div class=\"trash\" id=\"%s\" title=\"%s\">&nbsp;</div>", $row->name, lang('delete'));
 	$path = sprintf("/uploads/%s", $row->name);
+	$name = $row->name;
 	
-	$strings[] = sprintf("['<a href=\"/uploads/%s\" class=\"colorbox\">%s</a>','%s','%s','%s','%s']", $row->name, $row->name, $path, $row->size, $row->modified, $delete);
+	if(substr_count($row->type, 'image') > 0) {
+		$name = sprintf('<a href=\"/uploads/%s\" class=\"colorbox\">%s</a>', $row->name, $row->name);
+	}
+	
+	$strings[] = sprintf("['%s','%s','%s','%s','%s']", $name, $path, $row->size, $row->modified, $delete);
 }
 
 $output .= implode(",",$strings);	
